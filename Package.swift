b7ff89ba3868b25a6b728d4a940ce8b83681c980
eegrok <304507+eegrok@users.noreply.github.com>
2023-01-27 10:56:21 +1300

    add checksums for 10.33.0

--- Package.swift
+++ Package.swift
@@ -4,6 +4,8 @@
 import PackageDescription
 
 let realmVersion = "10.33.0"
+let realmChecksum = "3257ab9ab3e0e10d7b5d1986ae5700b31d3b0374af65a1364cf10799d20707b7"
+let realmSwiftChecksum = "0864e69564cb454cf299f2dc3c624b34387ea6a6d96aa4b9d4b3513d7ed91249"
 
 func buildTargets() -> [Target] {
     let baseURL = "https://github.com/atpsoft/RealmBinaries/releases/download/\(realmVersion)"
